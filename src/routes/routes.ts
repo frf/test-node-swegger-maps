1333016ddfb6e3767c385254e0766262bda628eb
Fabio <fabio@fabiofarias.com.br>
2020-08-31 00:23:29 -0300

    First

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,14 @@
 import express, { Request, Response } from 'express';
 import MapController from '../controllers/MapController';
+import SpendingController from '../controllers/SpendingController';
 
 const routes = express.Router();
 
 const mapController = new MapController();
+const spendingController = new SpendingController();
 
 routes.get('/markers', mapController.index);
+routes.get('/spendings', spendingController.index);
+routes.get('/spendings/show', spendingController.show);
 
 export default routes;
